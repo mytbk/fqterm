ad5f034e13797f1846d3f967e6db0f6137e1f1f3
Iru Cai <mytbk920423@gmail.com>
2014-05-03 20:55:35 +0800

    change code style

--- src/fqterm/main.cpp
+++ src/fqterm/main.cpp
@@ -123,13 +123,12 @@ int main(int argc, char **argv) {
     return -1;
   }
 
-  FQTermFrame *mw = new FQTermFrame();
-  mw->setWindowTitle("FQTerm " + QString(FQTERM_VERSION_STRING));
-  mw->setWindowIcon(QPixmap(getPath(RESOURCE) + "pic/fqterm.png"));
-  mw->show();
+  FQTermFrame mw;
+  mw.setWindowTitle("FQTerm " + QString(FQTERM_VERSION_STRING));
+  mw.setWindowIcon(QPixmap(getPath(RESOURCE) + "pic/fqterm.png"));
+  mw.show();
   a.setQuitOnLastWindowClosed(false);
-  FQ_VERIFY(a.connect(mw, SIGNAL(destroyed(QObject*)), &a, SLOT(mainWindowDestroyed(QObject*)), Qt::QueuedConnection));
-  FQ_VERIFY(a.connect(&a, SIGNAL(saveData()), mw, SLOT(saveSetting())));
-  int res = a.exec();
-  return res;
+  FQ_VERIFY(a.connect(&mw, SIGNAL(destroyed(QObject*)), &a, SLOT(mainWindowDestroyed(QObject*)), Qt::QueuedConnection));
+  FQ_VERIFY(a.connect(&a, SIGNAL(saveData()), &mw, SLOT(saveSetting())));
+  return a.exec();
 }
