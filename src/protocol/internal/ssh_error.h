535fc2e838277de6132ea1b3f5a3726c315e6d5e
Iru Cai <mytbk920423@gmail.com>
2018-05-17 12:14:41 +0800

    use ssh_error.h to define error numbers

---
+++ src/protocol/internal/ssh_error.h
@@ -0,0 +1,10 @@
+#ifndef SSH_ERROR_H
+#define SSH_ERROR_H
+
+enum {
+	ESECRET = 1,
+	ERSA,
+	ECRYPT,
+};
+
+#endif
--- src/protocol/internal/ssh_rsa.h
+++ src/protocol/internal/ssh_rsa.h
@@ -2,14 +2,13 @@
 #define SSH_RSA
 
 #include "ssh_session.h"
+#include "ssh_error.h"
 #include <stdlib.h>
 #include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif /* } */
-
-enum { ESECRET = 1, ERSA };
 
 int verifyRSAKey(ssh_session *ss, const uint8_t *data, size_t len);
 
